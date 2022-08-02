92ade208aa57de58933e5cf82558bf686fd932c0
Jenn Franco <jfranco376@gmail.com>
2022-08-02 12:25:07 -0500

    edits

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -1,42 +1,46 @@
-import React, {Component} from 'react';
-import ChildComponent from './../components/Footer/index';
-import MyConditionalComponent from '../components/MyConditionalComponent/index';
+import React, { Component } from "react";
+import ChildComponent from "./../components/Footer/index";
+import MyConditionalComponent from "../components/MyConditionalComponent/index";
+import NameForm from "../components/NameForm";
 // // import MyFirstComponent from '../MyFirstComponent';
 
 class Layout extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      food: 'it is a frog',
-      describe: 'chicken',
+      food: "it is a frog",
+      describe: "chicken",
       arrayOfTodos: [],
-      isVerified: false,
-    };    
+      isVerified: true,
+    };
   }
+
+  handleSubmitFood = (food) => {
+    this.setState({food});
+  };
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-     .then(response => {
-      return response.json()
-     }).then(arrayOfTodos => this.setState({ arrayOfTodos }))
- }
-
-    render() {
-      return (
-        <div>
-          <MyConditionalComponent isVerified={this.state.isVerified}/>
-          <ol>{this.state.arrayOfTodos.map((todo, index) => {
-          return (
-            <li key={index}>
-							{todo.title}
-						</li>
-          )
-        })}</ol>
-          <ChildComponent frog={this.state.food} chicken={this.state.describe}/>
-        </div>
-      );
-    }
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => {
+        return response.json();
+      })
+      .then((arrayOfTodos) => this.setState({ arrayOfTodos }));
   }
-  
+
+  render() {
+    return (
+      <div>
+        <ChildComponent frog={this.state.food} chicken={this.state.describe} />
+        <NameForm foodChange={this.handleSubmitFood} />
+        <MyConditionalComponent isVerified={this.state.isVerified} />
+        <ol>
+          {this.state.arrayOfTodos.map((todo, index) => {
+            return <li key={index}>{todo.title}</li>;
+          })}
+        </ol>
+      </div>
+    );
+  }
+}
 
 export default Layout;
