b5725cc93836099c05997a62c5e02bf6967f0a78
Jenn Franco <jfranco376@gmail.com>
2022-08-03 13:04:25 -0500

    adding buttons

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import "./App.css";
 import MyFirstClassComponent from "./components/MyFirstClassComponent/index";
 import Layout from "./containers/Layout";
-import {ExampleComponent, Hidden} from './components/FunctionalHook/index';
-import {MyfunctionTodos, MyCountComponent} from './components/FunctionalHook/indexUseEffect';
+import { ExampleComponent, Hidden } from "./components/FunctionalHook/index";
+import {
+  MyfunctionTodos,
+  MyCountComponent,
+} from "./components/FunctionalHook/indexUseEffect";
+import { Button } from "@mui/material";
+
 
 function App() {
   const message = true;
   return (
     <div className="App">
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+      <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
+      <Button variant="contained" color="primary">
+        Disabled
+      </Button>
       {/* <MyfunctionTodos/> */}
-      <MyCountComponent/>
+      {/* <MyCountComponent /> */}
       {/* <Hidden/> */}
-      {/* <ExampleComponent/>
-      <MyFirstClassComponent /> */}
-      {/* <Layout /> */}
+      <ExampleComponent />
+      <MyFirstClassComponent />
+      <Layout />
     </div>
   );
 }
